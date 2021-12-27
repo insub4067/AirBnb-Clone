b19ae779128e83a596c03a3b7234efb2c5e863bb
insub4067 <kim4067@stu.co.kr>
2021-12-28 02:21:53 +0900

    django messages

--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 import os
+from django.http import request
 import requests
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, reverse
 from django.urls import reverse_lazy
-from django.views.generic import FormView
-from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import FormView, DetailView
 from django.core.files.base import ContentFile
+from django.contrib import messages
+
+
 from . import models as users_models
 from . import forms
 
@@ -25,6 +28,7 @@ class LoginView(FormView):
 
 def log_out(request):
     logout(request)
+    messages.info(request, "다음에 또 만나요")
     return redirect(reverse("core:home"))
 
 
@@ -84,7 +88,7 @@ def github_callback(request):
             token_json = token.json()
             error = token_json.get("error", None)
             if error is not None:
-                raise GithubException()
+                raise GithubException("인증에 실패 하였습니다")
             else:
                 access_token = token_json.get("access_token")
                 profile_request = requests.get(
@@ -103,7 +107,7 @@ def github_callback(request):
                 try:
                     user = users_models.User.objects.get(email=email)
                     if user.login_method != users_models.User.LOGIN_GITHUB:
-                        raise GithubException()
+                        raise GithubException(f"{user.login_method}으로 시도해 주세요")
                 except users_models.User.DoesNotExist:
                     user = users_models.User.objects.create(
                         email=email,
@@ -116,14 +120,16 @@ def github_callback(request):
                     user.set_unusable_password()
                     user.save()
                 login(request, user)
+                messages.success(request, "에어비엔비에 오신 것을 환영합니다")
                 return redirect(reverse("core:home"))
 
             else:
-                raise GithubException()
+                raise GithubException("유저 정보를 가지고 오는데 실패했습니다")
 
         else:
-            raise GithubException()
-    except GithubException:
+            raise GithubException("로그인에 실패 했습니다")
+    except GithubException as e:
+        messages.error(request, e)
         return redirect(reverse("users:login"))
 
 
@@ -131,7 +137,7 @@ class KakaoException(Exception):
     pass
 
 
-def kakao_login(req):
+def kakao_login(request):
     REST_API_KEY = os.environ.get("KAKAO_REST_API_KEY")
     REDIRECT_URI = "http://127.0.0.1:8000/users/login/kakao/callback"
 
@@ -140,56 +146,65 @@ def kakao_login(req):
     )
 
 
-def kakao_callback(req):
-
-    code = req.GET.get("code")
-    grant_type = "authorization_code"
-    client_id = os.environ.get("KAKAO_REST_API_KEY")
-    redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
-    token = requests.post(
-        f"https://kauth.kakao.com/oauth/token?code={code}&grant_type={grant_type}&client_id={client_id}&redirect_uri={redirect_uri}",
-        headers={"application": "x-www-form-urlencoded;charset=utf-8"},
-    )
-
-    token_json = token.json()
-    error = token_json.get("error", None)
-
-    if error is not None:
-        raise KakaoException()
-    access_token = token_json.get("access_token")
-    profile_request = requests.post(
-        "https://kapi.kakao.com/v2/user/me",
-        headers={"Authorization": f"Bearer {access_token}"},
-    )
-    profile_json = profile_request.json()
-    kakao_account = profile_json.get("kakao_account")
-    email = kakao_account["email"]
-    if email is None:
-        raise KakaoException()
-    profile = kakao_account["profile"]
-    nickname = profile["nickname"]
-    profile_image_url = profile["profile_image_url"]
+def kakao_callback(request):
     try:
-        user = users_models.User.objects.get(email=email)
-        if user.login_method != users_models.User.LOGIN_KAKAO:
-            raise KakaoException()
-    except users_models.User.DoesNotExist:
-        user = users_models.User.objects.create(
-            email=email,
-            username=email,
-            first_name=nickname,
-            login_method=users_models.User.LOGIN_KAKAO,
-            email_verify=True,
+        code = request.GET.get("code") 
+        grant_type = "authorization_code"
+        client_id = os.environ.get("KAKAO_REST_API_KEY")
+        redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+        token = requests.post(
+            f"https://kauth.kakao.com/oauth/token?code={code}&grant_type={grant_type}&client_id={client_id}&redirect_uri={redirect_uri}",
+            headers={"application": "x-www-form-urlencoded;charset=utf-8"},
         )
-        user.set_unusable_password()
-        user.save()
-        if profile_image_url is not None:
-            photo_request = requests.get(profile_image_url)
-            user.avatar.save(
-                f"{nickname}_avatar",
-                ContentFile(photo_request.content),
-                save=True,
+
+        token_json = token.json()
+        error = token_json.get("error", None)
+
+        if error is not None:
+            raise KakaoException("카카오 인증에 실패 했습니다")
+        access_token = token_json.get("access_token")
+        profile_request = requests.post(
+            "https://kapi.kakao.com/v2/user/me",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+        profile_json = profile_request.json()
+        kakao_account = profile_json.get("kakao_account")
+        email = kakao_account["email"]
+        if email is None:
+            raise KakaoException("이메일을 입력해 주세요")
+        profile = kakao_account["profile"]
+        nickname = profile["nickname"]
+        profile_image_url = profile["profile_image_url"]
+        try:
+            user = users_models.User.objects.get(email=email)
+            if user.login_method != users_models.User.LOGIN_KAKAO:
+                raise KakaoException(f"{user.login_method}으로 시도해 주세요")
+        except users_models.User.DoesNotExist:
+            user = users_models.User.objects.create(
+                email=email,
+                username=email,
+                first_name=nickname,
+                login_method=users_models.User.LOGIN_KAKAO,
+                email_verify=True,
             )
+            user.set_unusable_password()
             user.save()
-    login(req, user)
-    return redirect(reverse("core:home"))
+            if profile_image_url is not None:
+                photo_request = requests.get(profile_image_url)
+                user.avatar.save(
+                    f"{nickname}_avatar",
+                    ContentFile(photo_request.content),
+                    save=True,
+                )
+                user.save()
+        login(request, user)
+        messages.success(request, "에어비엔비에 오신 것을 환영합니다")
+        return redirect(reverse("core:home"))
+    except KakaoException as e:
+        messages.error(request, e)
+        return redirect(reverse("users:login"))
+
+
+
+class UserProfileView(DetailView):
+    pass
